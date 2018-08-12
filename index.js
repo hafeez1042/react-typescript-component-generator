f100a655c8aa9936d7a0d1ed509ce448210895ed
Hafeez Hamza <hafeez1042@gmail.com>
2018-08-12 15:32:03 +0530

    env set

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const program = require('commander')
 const generateReactClassComponent = require('./generateReactClassComponent')
 const generateReactStatelessComponent = require('./generateReactStatelessComponent')
